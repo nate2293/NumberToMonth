9aac2548dc41eaa247a9a3bfd8ceedf30e82308f
nate2293 <Mc_Garvey-N@ulster.ac.uk>
2024-01-10 16:39:51 +0000

    Add project files.

---
+++ NumberToMonth/NumMonthName.cs
@@ -0,0 +1,33 @@
+﻿namespace NumberToMonth
+{
+    internal class NumMonthName
+    {
+        static void Main()
+        {
+            Console.WriteLine(MonthName(3));    // Output: "March"
+            Console.WriteLine();
+            Console.WriteLine(MonthName(12));   // Output: "December"
+            Console.WriteLine();
+            Console.WriteLine(MonthName(6));    // Output: "June"
+            Console.WriteLine();
+        }
+
+        static string MonthName(int number)
+        {
+            string[] months = {
+            "January", "February", "March", "April",
+            "May", "June", "July", "August",
+            "September", "October", "November", "December"
+        };
+
+            if (number >= 1 && number <= 12)
+            {
+                return months[number - 1];
+            }
+            else
+            {
+                return "Invalid month number. Please enter a number between 1 and 12.";
+            }
+        }
+    }
+}
